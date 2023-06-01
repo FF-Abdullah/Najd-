283f9042e5d4f67439ec0dffe43b225a40a5da0a
FF-Abdullah <111127350+FF-Abdullah@users.noreply.github.com>
2023-06-01 16:37:12 +0300

    Add files via upload

--- src/init-oauth.php
+++ src/init-oauth.php
@@ -1,6 +1,6 @@
 <?php
 
-$discord_url = "{https://discord.com/api/oauth2/authorize?client_id=933643339755032577&redirect_uri=https%3A%2F%2Flocalhost%2Fyt-discord%2Fsrc%2Fprocess-oauth.php&response_type=code&scope=identify%20rpc.voice.read}";
+$discord_url = "{REDIRECT_URL_LINK_FROM_DASHBOARD}";
 header("Location: $discord_url");
 exit();
 
